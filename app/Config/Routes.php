c0d4d337578af56eac32514f2a98efb56aedebb6
rafasepti <rafasepti@gmail.com>
2025-05-08 12:42:10 +0700

    add: style login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+//$routes->get('/', 'Home::index');
+$routes->get('/', function () {
+    return view('login');
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
       './node_modules/flowbite/**/*.js'
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          primary: {
+            blue: '#4976a6',
+            "blue-200": '#3f5291',
+            yellow: '#ffb155',
+          }
+        }
+      },
     },
     plugins: [
       require('flowbite/plugin')
